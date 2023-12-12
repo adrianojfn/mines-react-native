9d2b2259f31ded0bdc9825d11b571ebfb00871c1
Adriano França <adrianojfn@gmail.com>
2023-12-12 00:41:47 -0300

    Field component and your respective colors

--- App.js
+++ App.js
@@ -1,11 +1,19 @@
 import { StyleSheet, Text, View } from 'react-native';
 import params from './src/params'
+import Field from './src/components/Field';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Iniciando o Mines!</Text>
-      <Text>Tamanho da grade: {params.getRowsAmount()}x{params.getColumnsAmount()}</Text>
+      <Text>Starting the Mines!</Text>
+      <Text>Grid size: {params.getRowsAmount()}x{params.getColumnsAmount()}</Text>
+
+      <Field />
+      <Field opened />
+      <Field opened nearMines={1}/>
+      <Field opened nearMines={2}/>
+      <Field opened nearMines={3}/>
+      <Field opened nearMines={6}/>
     </View>
   );
 }
