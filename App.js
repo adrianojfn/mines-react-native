81d912982bb51a602a1d2ca766a8f6fa6c1166ac
Adriano França <adrianojfn@gmail.com>
2023-12-21 00:40:10 -0300

    LevelSelection component
    A modal to select the game difficulty

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import params from './src/params'
 import MineField from './src/components/MineField';
 import { createMinedBoard, cloneBoard, openField, hadExplosion, showMines, wonGame, invertFlag, flagsUsed } from './src/logic'
 import Header from './src/components/Header';
+import LevelSelection from './src/screens/LevelSelection';
 
 export default class App extends Component {
 
@@ -24,7 +25,8 @@ export default class App extends Component {
     return {
       board: createMinedBoard(rows, cols, this.minesAmount()),
       won: false,
-      lost: false
+      lost: false,
+      showLevelSelection: false,
     }
   }
 
@@ -58,11 +60,17 @@ export default class App extends Component {
     this.setState({board, won})
   }
 
+  onLevelSelected = level => {
+    params.difficultyLevel = level
+    this.setState(this.createState())
+  }
+
   render() {
     return (
       <View style={styles.container}>
+        <LevelSelection isVisible={this.state.showLevelSelection} onLevelSelected={this.onLevelSelected} onCancel={() => this.setState({ showLevelSelection: false })} />
         <Header flagsLeft={this.minesAmount() - flagsUsed(this.state.board)}
-        onNewGame={() => this.setState(this.createState())} />
+        onNewGame={() => this.setState(this.createState())} onFlagPress={() => this.setState({ showLevelSelection: true })} />
         <View style={styles.board}>
           <MineField board={this.state.board} onOpenField={this.onOpenField} onSelectField={this.onSelectField}/>
         </View>
